8f5439e9241b16e01c98a60c853b0ec4093be41d
POBIX <someonesomebody34@gmail.com>
2022-11-28 13:25:08 +0200

    Added timer event

--- src/timer.rs
+++ src/timer.rs
@@ -1,5 +1,7 @@
 use crate::interrupts::GateType;
 use crate::{interrupts, pic};
+use spin::Mutex;
+use crate::events::{Event, EventHandler};
 
 pub fn init() {
     unsafe {
@@ -10,6 +12,13 @@ pub fn init() {
 }
 
 extern "x86-interrupt" fn on_tick() {
+    unsafe { TIMER += 1; }
+    ON_TICK.lock().invoke(());
     pic::send_eoi(0);
 }
 
+#[inline]
+pub fn get_ticks() -> u64 { unsafe { TIMER } }
+
+pub static ON_TICK: Mutex<Event<()>> = Mutex::new(Event::<>::new());
+static mut TIMER: u64 = 0;
