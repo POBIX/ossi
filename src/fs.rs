d61c7285cc0cf8e0b01221b19e39cd759f6fa433
POBIX <someonesomebody34@gmail.com>
2023-01-02 07:50:40 +0200

    Opening and closing files, minor refactors

--- src/fs.rs
+++ src/fs.rs
@@ -1,6 +1,6 @@
 use core::mem::size_of;
 
-use spin::{Mutex, Lazy};
+use spin::{Lazy, Mutex};
 
 /// The number of sectors the Header struct takes up.
 /// Maximum number of files = HEADER_SECTORS * 512 / size_of::<FileMetadata>() - 1
@@ -21,13 +21,7 @@ struct FileMetadata {
     /// how many sectors does this file take up?
     size: usize,
     /// is this file currently open
-    opened: bool
-}
-
-impl FileMetadata {
-    const fn null() -> FileMetadata {
-        FileMetadata { path: [0; MAX_PATH_LENGTH], sector: 0, size: 0, opened: false }
-    }
+    opened: bool,
 }
 
 /// The struct that sits at the top of the hard drive, containing the FileMetadata maps.
@@ -35,12 +29,11 @@ impl FileMetadata {
 struct Header {
     first_null: usize, // the index (in entries, the next field) of the first null FileMetadata.
     entries: [FileMetadata; MAX_FILES],
-    _padding: [u8; 1024 - 988] // align to 512 bytes
+    _padding: [u8; 1024 - 988], // align to 512 bytes
 }
 
 pub struct File {
-    address: usize,
-    path: &'static str
+    index: usize,
 }
 
 #[derive(Debug)]
@@ -55,26 +48,38 @@ pub enum FileError {
 }
 
 impl File {
-    // pub fn open(path: &str) -> Result<File, FileError> {
+    pub fn open(path: &str) -> Result<File, FileError> {
+        let mut header = HEADER.lock();
 
-    // }
-
-    pub fn close(&mut self) {
-
+        // search for a file with the same path as the parameter
+        for i in 0..header.entries.len() {
+            let file = &mut header.entries[i];
+            // convert the path (a null terminated [u8]) to a rust string
+            let file_path = unsafe {
+                core::str::from_utf8_unchecked(
+                    core::ffi::CStr::from_ptr(file.path.as_ptr() as *const _).to_bytes(),
+                )
+            };
+            if file_path == path {
+                // found it!
+                if file.opened {
+                    return Err(FileError::FileAlreadyOpen);
+                }
+                file.opened = true;
+                return Ok(File { index: i });
+            }
+        }
+        Err(FileError::FileNotFound)
     }
 
-    // pub fn read(&self) -> &[u8] {
-
-    // }
-
-    pub fn write(&mut self, data: &[u8]) {
-
+    pub fn close(&mut self) {
+        let mut header = HEADER.lock();
+        header.entries[self.index].opened = false;
+        self.index = MAX_FILES+1; // mark this reference as invalid
     }
 
     pub fn create(path: &'static str) -> Result<File, FileError> {
         let mut header = HEADER.lock();
-        let test_header = header.first_null;
-        let test2 = header.entries;
 
         if path.len() >= MAX_PATH_LENGTH {
             return Err(FileError::PathTooLong);
@@ -86,9 +91,13 @@ impl File {
         // header.first_null is the index of the file we're going to create.
         // in order to figure out which sector we should write to,
         // we simply add one sector to the previous file's sector.
-        // (note: the first file should always exist, but be empty and null, because of the index)
-        let prev_entry = header.entries[header.first_null - 1];
-        let addr: usize = prev_entry.sector + prev_entry.size;
+        let addr: usize = if header.first_null > 0 {
+            let prev = header.entries[header.first_null];
+            prev.sector + prev.size
+        } else {
+            // or, if this is the first file, use the first available sector.
+            HEADER_SECTORS + 1
+        };
 
         // pad the path with nulls to the right
         let mut padded_path: [u8; MAX_PATH_LENGTH] = [0; MAX_PATH_LENGTH];
@@ -103,22 +112,15 @@ impl File {
             size: 1,
             opened: false,
         };
+        header.first_null += 1;
 
-        unsafe {
-            // update the header on disk.
-            // we transmute header to be a byte array, and we use *& in order to get the data behind the mutex.
-            crate::ata::write_sectors(0, core::mem::transmute(&*header), HEADER_SECTORS);
-        }
+        // update it on disk
+        update_header(&header);
 
-        Ok(File {
-            address: addr,
-            path
-        })
+        Ok(File { index: first_null })
     }
 
-    pub fn delete(&mut self) {
-
-    }
+    pub fn delete(&mut self) {}
 }
 
 fn read_header() -> Mutex<Header> {
@@ -130,6 +132,15 @@ fn read_header() -> Mutex<Header> {
     }
 
     Mutex::new(header)
+}
+
+/// writes header to disk.
+#[inline]
+fn update_header(header: &Header) {
+    unsafe {
+        // we transmute header to be a byte array, and we use *& in order to get the data behind the mutex.
+        crate::ata::write_sectors(0, core::mem::transmute(header), HEADER_SECTORS);
+    }
 }
 
 static HEADER: Lazy<Mutex<Header>> = Lazy::new(read_header);
--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,8 @@ pub(crate) extern "C" fn main(info: &grub::MultibootInfo, magic: u32) -> ! {
     console::init();
     ata::init();
 
-    let file = fs::File::create("/my/nice/file.txt").unwrap();
+    let file = fs::File::open("/my/nice/file.txt").unwrap();
+    println!("literally what the FUCK dude");
 
     loop {
         unsafe {
